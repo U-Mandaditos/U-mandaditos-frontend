d273b7d72e697a617e7e686b43f05fb312b9f48a
Daniel <danyochoa112@gmail.com>
2025-03-04 22:42:02 -0600

    feat: creación de componente SlidingPanel

--- src/app/ui/essentials/Paragraph.jsx
+++ src/app/ui/essentials/Paragraph.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 const ParagraphStyled = styled.p`
   font-size: ${props => props.size || '1rem'}; 
   font-weight: ${props => props.weight || 'normal'};
-  color: ${props => props.color || props.theme.colors.secondaryText};
+  color: ${props => props.color || props.theme.colors.foreground};
 `;
 
-export default function Paragraph({children,size,weight,color, className}){
-    return(<ParagraphStyled size={size} weight={weight} color={color} className={className}>{children}</ParagraphStyled>);
+export default function Paragraph({text,size,weight,color, className}){
+    return(<ParagraphStyled size={size} weight={weight} color={color} className={className}>{text}</ParagraphStyled>);
 }
