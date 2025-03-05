5c07396b7f95c06e48feecbc97b1622c1cad9b22
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2025-03-04 23:10:03 -0600

    Componente de DeliveryCard

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
     setInputValue(event.target.value);
   };
 
-  const router = useRouter();
 
   return (
     <div className="p-4">
--- src/app/ui/cards/DeliveryCard.jsx
+++ src/app/ui/cards/DeliveryCard.jsx
@@ -1,5 +1,8 @@
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { FlexContainer } from '../essentials/FlexBox';
+import RunnerIcon from "/public/img/runner-icon.svg";
+import Next from "/public/img/play-arrow.svg";
+import Location from "/public/img/location-icon.svg";
 
 const ContainerGeneral = styled.div`
     width: auto;
@@ -9,19 +12,93 @@ const ContainerGeneral = styled.div`
     background-color: transparent;
     display: flex;
     flex-direction: row;
+    padding: 10px 10px 0 10px;
 `;
 
-const RContainer = styled.div`
-    width: 70%;
-    height: 100%;
+const Icon = styled.svg`
+  width: ${(props) => props.width || '14px'};
+  height: auto;
+  path {
+    fill: ${(props) => props.color || props.theme.colors.foreground}; 
+  }
+`;
+
+const Card = styled.div`
+    width: auto;
+    height: auto;
     display: flex;
-    flex-direction: column;
+    gap: 5px;
+    background-color: ${(props) => props.color || props.theme.colors.secondary};
+    border-radius: 4px;
+    padding: 4px 6px 4px 6px;
+    align-items: center;
+    flex-direction: row;
+    color: ${(props) => props.theme.colors.main};
+    font-size: 10px;
+    margin: 0 2px 0 2px;
+`;
+
+const TextCard = styled.span`
+    font-size: ${(props) => props.size || '12px'};
+    color: ${(props) => props.color || props.theme.colors.secondaryText};
+    font-weight: ${(props) => props.weight || 400};
+`;
+
+const StatusCard = styled.div`
+    width: min-content;
+    height: auto;
+    display: flex;
+    white-space: nowrap;
+    background-color: transparent;
+    border-radius: 4px;
+    padding: 2px 5px 2px 5px;
+    font-weight: 400;
+    color: ${(props) => 
+        props.status === 1 ? props.theme.colors.tertiary :
+        props.status === 2 ? props.theme.colors.secondary :
+        props.theme.colors.secondaryText};
+    font-size: 10px;
+    margin-top: 3px;
+    border: 1px solid ${(props) => 
+        props.status === 1 ? props.theme.colors.tertiary :
+        props.status === 2 ? props.theme.colors.secondary :
+        props.theme.colors.lineColor};
 `;
 
 
 export default function DeliveryCard ({}) {
+    const theme = useTheme();
     return (
         <ContainerGeneral>
+            <FlexContainer width={"75%"} height="100%" direction="column" gap="2px">
+                <FlexContainer height="auto" direction="row" alignitems="center" gap="5px" className='mb-2'>
+                    <Icon as={RunnerIcon}></Icon>
+                    <Icon as={Next} width="5px"></Icon>
+                    <Card>
+                        <Icon as={Location} width="10px" color={theme.colors.main}/>
+                        B2
+                    </Card>
+                    <Icon as={Next} width="5px"></Icon>
+                    <Card color={theme.colors.tertiary}>
+                        <Icon as={Location} width="10px" color={theme.colors.main}/>
+                        Polideportivo
+                    </Card>
+                </FlexContainer>
+                <FlexContainer direction="row" gap="4px">
+                    <TextCard weight="500">Mandadito:</TextCard>
+                    <TextCard>Alitas del CC</TextCard>
+                </FlexContainer>
+                <FlexContainer direction="row" gap="4px">
+                    <TextCard weight="500">Entrega:</TextCard>
+                    <TextCard>Angel Fernando Castillo</TextCard>
+                </FlexContainer>
+                <StatusCard status={2}>En progreso</StatusCard>
+
+            </FlexContainer>
+            <FlexContainer direction="column" alignitems="flex-end" gap="3px">
+                <TextCard color={theme.colors.foreground} weight="500" size="16px">3:23 pm</TextCard>
+                <TextCard color={theme.colors.secondaryText} weight="400" size="12px">L 20.00</TextCard>
+            </FlexContainer>
 
         </ContainerGeneral>
     )
