53787ab893edff969a70aec2b9cb8104e54fb92a
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2025-03-06 12:10:06 -0600

    feat: Componente PostDeliveryCard

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,8 @@ import Title from "./ui/essentials/Title";
 import Paragraph from "./ui/essentials/Paragraph";
 import Header from "./ui/utilities/Header"; 
 import { useRouter } from "next/navigation"; 
-import IconButton from "./ui/buttons/IconButton";
-import OfferCard from "./ui/cards/OfferCard";
-import ReviewCard from "./ui/cards/ReviewCard";
-import SendInput from "./ui/utilities/SendInput";
-import LocationSelect from "./ui/utilities/LocationSelect";
-import { review, user, location } from "../../data";
 import DeliveryCard from "./ui/cards/DeliveryCard";
+import PostDeliveryCard from "./ui/cards/PostDeliveryCard";
 
 
 export default function Home() {
@@ -34,10 +29,6 @@ export default function Home() {
     <>
       <Header text="U mandaditos" />
 
-      <AccessButton icon={"sendMail"} text={"Tus mandaditos"}/>
-      <AccessButton icon={"runnerIcon2"} text={"Tus entregas"}/>
-      <AccessButton icon={"reviews"} text={"Tus reseñas"}/>
-
 
       <div className="p-4">
         <Title text="U mandaitos" />
@@ -54,7 +45,7 @@ export default function Home() {
       />
 
 
-<DeliveryCard />
+<PostDeliveryCard />
       </div>
     </>
   );
