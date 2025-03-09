8233ec0d5709c29f563a5301dcef2646922ba15f
Kattherine Hernandez <katerinmayely@gmail.com>
2025-03-09 02:09:31 -0600

    fix: lá página de inicio de sesión ahora dentro de etiqueta form

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import styled from 'styled-components';
 import Button from "../ui/essentials/Button"
 import { FlexContainer} from "../ui/essentials/FlexBox"
 import Input from "../ui/essentials/Input"
@@ -8,26 +9,37 @@ import Title from "../ui/essentials/Title"
 import Link from "../ui/essentials/Link"
 import AuthenticationHeader from "../ui/utilities/AuthenticationHeader"
 
-export default function Page(){
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    gap: 2rem;
+    padding: 2rem;
+`;
+
+export default function Page() {
     return (
-        <FlexContainer className="p-5" direction="column" justifycontent="center" alignitems="start" gap="3rem">
-            <AuthenticationHeader title={"Iniciar sesión"} text={"Bienvenido de vuelta a U-Mandaditos"} />
-            <div> 
-                <Title className={"mb-2"} weight={"strong"} size={"medium"} text="Correo" />
-                <Input name={"email"} required={false} type={"email"} width={"314px"} placeholder="e.g afcastillof@unah.hn" />
-            </div>
-            <div>
-                <Title className={"mb-2"} weight={"strong"} size={"medium"} text="Contraseña" />
-                <Input name={"password"} type={"password"} required={false} width={"314px"} placeholder="Ingresa tu contraseña" />
-                <Link text={"¿Olvidaste tu contraseña?"} color={(props) => props.theme.colors.secondaryText} size={"13px"} weight={"400"} float={"right"} href={"#"}/>
-            </div>   
-            <FlexContainer direction="column" alignitems="center">
-                <Button borderRadius={"30px"} width={"300px"} text={"Iniciar sesión"}/>
-            </FlexContainer>   
-            <FlexContainer direction="row" alignitems={"center"} justifycontent="center" gap="5px">
-                <Paragraph color={(props) => props.theme.colors.secondaryText} weight={"600"} size={"13px"} text={"¿Todavía no tienes una cuenta?"}/> 
-                <Link href="/register" text={"Regístrate"} color={(props) => props.theme.colors.primary} size={"13px"} weight={"600"} float={"none"}/>
-            </FlexContainer>      
-        </FlexContainer>
-    )
+        <>
+            <AuthenticationHeader title={"Inicia sesión"} text={"Bienvenido de vuelta a U-Mandaditos"} />
+            <StyledForm>
+                <div> 
+                    <Title className={"mb-2"} weight={"strong"} size={"medium"} text="Correo" />
+                    <Input name={"email"} required={false} type={"email"} width={"314px"} placeholder="e.g afcastillof@unah.hn" />
+                </div>
+                <div>
+                    <Title className={"mb-2"} weight={"strong"} size={"medium"} text="Contraseña" />
+                    <Input name={"password"} type={"password"} required={false} width={"314px"} placeholder="Ingresa tu contraseña" />
+                    <Link text={"¿Olvidaste tu contraseña?"} color={(props) => props.theme.colors.secondaryText} size={"13px"} weight={"400"} float={"right"} href={"#"} />
+                </div>   
+                <FlexContainer direction="column" alignitems="center">
+                    <Button borderRadius={"30px"} width={"300px"} text={"Iniciar sesión"} />
+                </FlexContainer>   
+                <FlexContainer direction="row" alignitems={"center"} justifycontent="center" gap="5px">
+                    <Paragraph color={(props) => props.theme.colors.secondaryText} weight={"600"} size={"13px"} text={"¿Todavía no tienes una cuenta?"}/> 
+                    <Link href="/register" text={"Regístrate"} color={(props) => props.theme.colors.primary} size={"13px"} weight={"600"} float={"none"} />
+                </FlexContainer>      
+            </StyledForm>
+        </>
+    );
 }
