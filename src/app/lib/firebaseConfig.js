bb44e8ef1ede584ff275c41bbf8d39e20d3d2541
Daniel <danyochoa112@gmail.com>
2025-03-23 17:47:29 -0600

    fix: se colocan las credenciales de firebase desde variables de entorno para mayor seguridad

--- src/app/lib/firebaseConfig.js
+++ src/app/lib/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxbftAfZz2umkz_1M-udnFVlHTyn-HkIQ",
-  authDomain: "umandaditos-chat.firebaseapp.com",
-  projectId: "umandaditos-chat",
-  storageBucket: "umandaditos-chat.firebasestorage.app",
-  messagingSenderId: "219634836770",
-  appId: "1:219634836770:web:71cfbdfb3fc7420e80f17b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
